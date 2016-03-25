ec813966641279d5384c230aa9bbfb22a455d351
Zhassulan Zhaksylyk <zzmj95@gmail.com>
2016-03-25 23:04:53 +0600

    Page changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -12,12 +12,7 @@
 
                 <div class="col-xs-12 col-sm-12 col-md-6 item">
                     <ul class="menu-list">
-                        <li><a data-video-href="../video/Nature Is Speaking- Edward Norton is The Soil - 大自然在說話- 愛德華諾頓聲演「土壤」- 保護國際基金會 (CI).mp4" href="/about">Interior Design</a></li>
-                        <li><a data-video-href="../video/Villa Altaïr - Real Estate Movie (Modern Architecture Luxury Villa) (online-video-cutter.com) (1).mp4" href="/about">Brand & Logos</a></li>
-                        <li><a data-video-href="../video/Villa Altaïr - Real Estate Movie (Modern Architecture Luxury Villa) (online-video-cutter.com).mp4" href="/about">Architecture design</a></li>
-                        <li><a data-video-href="../video/Фильм 05.mp4" href="/about">Furniture & Accessories</a></li>
-                        <li><a data-video-href="../video/Zaha Hadid- Form in Motion.mp4" href="/about">Drawing courses</a></li>
-                        <li>-------</li>
+                        {{--<li><a data-video-href="../video/Nature Is Speaking- Edward Norton is The Soil - 大自然在說話- 愛德華諾頓聲演「土壤」- 保護國際基金會 (CI).mp4" href="/about">Interior Design</a></li>--}}
                         @foreach($categories as $category)
                             <li>
                                 <a href="{{ url('page/' . $category->url) }}">
@@ -26,20 +21,6 @@
                             </li>
                         @endforeach
                     </ul>
-                    {{--@foreach(array_chunk($categories->all(), 3) as $category_rows)
-                        <div class="square-blocks clearfix">
-                            @foreach($category_rows as $category)
-                                <a href="{{ url('page/' . $category->url) }}" class="square" style="box-shadow: 0 0 0 1px #000;">
-                                    <div class="letter" style="color:black">
-                                        {{ ucfirst(substr($category->url, 0, 1)) }}
-                                    </div>
-                                    <div style="width: 112px; height: 112px;"
-                                         data-vide-bg="/video/ocean" data-vide-options="loop: true, muted: false, position: 0% 0%">
-                                    </div>
-                                </a>
-                            @endforeach
-                        </div>
-                    @endforeach--}}
                 </div>
             </div>
         </div>
--- resources/views/pages/page.blade.php
+++ resources/views/pages/page.blade.php
@@ -256,11 +256,9 @@
                     <div class="row what-we-take">
                         @foreach($process_rows as $process)
                             <div class="col-md-4 text-center">
-                                <a class="md-trigger" data-modal="modal-1">
+                                <a class="md-trigger" data-modal="processes-{{ $process->id }}">
                                     <img src="{{ $process->image }}" alt="" class="img-responsive">
-                                    <a class="md-trigger" data-modal="processes">
-                                        {{ $process->name }}
-                                    </a>
+                                    {{ $process->name }}
                                 </a>
                             </div>
                         @endforeach
@@ -275,9 +273,11 @@
             <div class="block-abs">
                 <div id="project_swiper_slider__js" class="swiper-container project-slider">
                     <div class="swiper-wrapper">
-                        @foreach($category->projects()->sorted() as $project)
+                        @foreach($category->projects()->sorted()->get() as $project)
                             <div class="swiper-slide">
-                                <img class="img-responsive" src="{{ $project->preview }}" alt="project">
+                                <a class="md-trigger" data-modal="projects-{{ $project->id }}">
+                                    <img class="img-responsive" src="{{ $project->preview }}" alt="project">
+                                </a>
                             </div>
                         @endforeach
                     </div>
@@ -297,17 +297,18 @@
                 <div id="reviews_swiper_slider__js" class="swiper-container reviews-slider">
                     <div class="swiper-wrapper">
                         @foreach($category->reviews()->sorted() as $review)
-                            <div class="swiper-slide"> 
-                                <div class="r_avatar" style="background-image:url({{ $review->avatar }})">
-                                </div>
-                                <h1 class="client_review" align="center">{{ $review->heading }}</h1>
-                                <hr>
-                                <h4 class="client_name" align="center">{{ $review->name }}</h4>
-                                <p align="center">
-                                    {!! $review->text !!}
-                                </p>
-
-                                <div class="date" align="center">{{ date('d.m.Y', strtotime($review->created_at)) }}</div>
+                            <div class="swiper-slide">
+                                <a class="md-trigger" data-modal="reviews-{{ $review->id }}">
+                                    <div class="r_avatar" style="background-image:url({{ $review->avatar }})">
+                                    </div>
+                                    <h1 class="client_review" align="center">{{ $review->heading }}</h1>
+                                    <hr>
+                                    <h4 class="client_name" align="center">{{ $review->name }}</h4>
+                                    <p align="center">
+                                        {!! $review->text !!}
+                                    </p>
+                                    <div class="date" align="center">{{ date('d.m.Y', strtotime($review->created_at)) }}</div>
+                                </a>
                             </div>
                         @endforeach
                     </div>
@@ -330,30 +331,32 @@
                             <div class="swiper-slide"> 
                                 @foreach($question_row as $item)
                                     <div class="row answer_question">
-                                        <div class="col-md-6 item">
-                                            <div class="answer block-item">
-                                                <h3>Вопрос</h3>
-                                                <p>
-                                                    {!! $item->question !!}
-                                                </p>
+                                        <a class="md-trigger" data-modal="questions-{{ $item->id }}">
+                                            <div class="col-md-6 item">
+                                                <div class="answer block-item">
+                                                    <h3>Вопрос</h3>
+                                                    <p>
+                                                        {!! $item->question !!}
+                                                    </p>
 
-                                                <div class="name">
-                                                    {{ $item->questioner }} {{ date('d.m.Yг.', strtotime($item->created_at)) }}
+                                                    <div class="name">
+                                                        {{ $item->questioner }} {{ date('d.m.Yг.', strtotime($item->created_at)) }}
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-6 item">
-                                            <div class="question block-item">
-                                                <h3>Ответ</h3>
-                                                <p>
-                                                    {!! $item->answer !!}
-                                                </p>
+                                            <div class="col-md-6 item">
+                                                <div class="question block-item">
+                                                    <h3>Ответ</h3>
+                                                    <p>
+                                                        {!! $item->answer !!}
+                                                    </p>
 
-                                                <div class="name">
-                                                    IlyasKali.com {{ date('d.m.Yг.', strtotime($item->created_at)) }}
+                                                    <div class="name">
+                                                        IlyasKali.com {{ date('d.m.Yг.', strtotime($item->created_at)) }}
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </a>
                                     </div>
                                 @endforeach
                             </div>
@@ -380,7 +383,7 @@
                                     <div class="row what-we-take">
                                         @foreach($blog_row as $blog)
                                             <div class="col-md-4 text-center">
-                                                <a class="md-trigger" data-modal="modal-1">
+                                                <a class="md-trigger" data-modal="blog-{{ $blog->id }}">
                                                     <img src="{{ $blog->preview }}" alt="" class="img-responsive">
                                                     <a>{{ $blog->title }}</a>
                                                 </a>
@@ -412,7 +415,7 @@
                                     <div class="row what-we-take">
                                         @foreach($partner_row as $partner)
                                             <div class="col-md-2 text-center">
-                                                <a class="md-trigger" data-modal="modal-1">
+                                                <a class="md-trigger" data-modal="partners-{{ $partner->id }}">
                                                     <img src="{{ $partner->image }}" alt="" class="img-responsive">
                                                 </a>
                                             </div>
@@ -432,45 +435,152 @@
         </section>
         <!-- End section4 section -->
     </main>
-    
-    <div class="md-modal md-effect-12" id="modal-1">
-        <div class="md-content">
-            <h3>Задание на проектирование</h3>
-            <div>
-                <p>This is a modal window. You can do the following things with it:</p>
-                <img src="/img/sl1.jpg">
-                <ul>
-                    <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
-                    <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
-                    <li><strong>Close:</strong> click on the button below to close the modal.</li>
-                </ul>
-                <button class="md-close"></button>
-                <div class="navigation">
-                    <div class="modal-arrow prev-modal"></div>
-                    <div class="modal-arrow next-modal"></div>
+
+    {{-- Processes --}}
+    @foreach($category->processes()->sorted()->get() as $process)
+        <div class="md-modal md-effect-12" id="processes-{{ $process->id }}">
+            <div class="md-content">
+                <h3>Задание на проектирование</h3>
+                <div>
+                    <p>PROCESS</p>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <img src="/img/sl1.jpg">
+                    <ul>
+                        <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+                        <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close"></button>
+                    <div class="navigation">
+                        <div class="modal-arrow prev-modal"></div>
+                        <div class="modal-arrow next-modal"></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="md-modal md-effect-12" id="modal-2">
-        <div class="md-content">
-            <h3>Задание на проектирование 2</h3>
-            <div>
-                <p>This is a modal window. You can do the following things with it:</p>
-                <img src="/img/sl1.jpg">
-                <ul>
-                    <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
-                    <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
-                    <li><strong>Close:</strong> click on the button below to close the modal.</li>
-                </ul>
-                <button class="md-close"></button>
-                <div class="navigation">
-                    <div class="modal-arrow prev-modal"></div>
-                    <div class="modal-arrow next-modal"></div>
+    @endforeach
+    {{-- Processes --}}
+
+    {{-- Projects --}}
+    @foreach($category->projects()->sorted()->get() as $project)
+        <div class="md-modal md-effect-12" id="projects-{{ $project->id }}">
+            <div class="md-content">
+                <h3>Задание на проектирование 2 projects</h3>
+                <div>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <img src="/img/sl1.jpg">
+                    <ul>
+                        <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+                        <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close"></button>
+                    <div class="navigation">
+                        <div class="modal-arrow prev-modal"></div>
+                        <div class="modal-arrow next-modal"></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endforeach
+    {{-- Projects --}}
+
+    {{-- Reviews --}}
+    @foreach($category->reviews()->sorted()->get() as $review)
+        <div class="md-modal md-effect-12" id="reviews-{{ $review->id }}">
+            <div class="md-content">
+                <h3>Задание на проектирование 2</h3>
+                <div>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <img src="/img/sl1.jpg">
+                    <ul>
+                        <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+                        <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close"></button>
+                    <div class="navigation">
+                        <div class="modal-arrow prev-modal"></div>
+                        <div class="modal-arrow next-modal"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    {{-- Reviews --}}
+
+    {{-- Questions --}}
+    @foreach($category->questions()->sorted()->get() as $question)
+        <div class="md-modal md-effect-12" id="questions-{{ $question->id }}">
+            <div class="md-content">
+                <h3>Задание на проектирование 2</h3>
+                <div>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <img src="/img/sl1.jpg">
+                    <ul>
+                        <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+                        <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close"></button>
+                    <div class="navigation">
+                        <div class="modal-arrow prev-modal"></div>
+                        <div class="modal-arrow next-modal"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    {{-- Questions --}}
+
+    {{-- Blog --}}
+    @foreach($category->blogs()->sorted()->get() as $blog)
+        <div class="md-modal md-effect-12" id="blog-{{ $blog->id }}">
+            <div class="md-content">
+                <h3>Задание на проектирование 2</h3>
+                <div>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <img src="/img/sl1.jpg">
+                    <ul>
+                        <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+                        <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close"></button>
+                    <div class="navigation">
+                        <div class="modal-arrow prev-modal"></div>
+                        <div class="modal-arrow next-modal"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    {{-- Blog --}}
+
+    {{-- Partners --}}
+    @foreach($category->partners()->sorted()->get() as $partner)
+        <div class="md-modal md-effect-12" id="partners-{{ $partner->id }}">
+            <div class="md-content">
+                <h3>Задание на проектирование 2</h3>
+                <div>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <img src="/img/sl1.jpg">
+                    <ul>
+                        <li><strong>Read:</strong> modal windows will probably tell you something important so don't forget to read what they say.</li>
+                        <li><strong>Look:</strong> a modal window enjoys a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close"></button>
+                    <div class="navigation">
+                        <div class="modal-arrow prev-modal"></div>
+                        <div class="modal-arrow next-modal"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+    {{-- Partners --}}
+
     <div class="md-overlay"></div><!-- the overlay element -->
     <div class="loader"></div>
 @stop
